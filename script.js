459902431aa88ba7b7f3dd224385ee61d51b6465
miguel <larchermgl@gmail.com>
2022-08-15 14:12:14 +0100

    Add functionality for numbered button and the clear and delete button

--- script.js
+++ script.js
@@ -5,22 +5,65 @@ buttons.forEach(button => button.addEventListener("click", e => {
     let buttonContent = button.textContent;
     let displayContent = display.textContent;
     if (!button.classList.contains("operator")) {
-        if (display.classList.contains("hasContent")) {
-            console.log(buttonContent);
-            display.textContent += buttonContent;
-            console.log(displayContent);
-        }
-        else {
-            display.textContent = buttonContent;
-            display.classList.add("hasContent");
-        }
-
+        displayNumber(button);
     }
     else if (buttonContent == "AC") {
-        displayContent = "0";
-        display.classList.remove("hasContent");
+        reset();
     }
     else if (buttonContent == "DEL") {
-        displayContent = displayContent.slice(0, displayContent.length - 2);
+        if (displayContent.length == 1 || displayContent.length == "0"){
+            reset();
+        }
+        else {
+            displayContent = displayContent.slice(0, displayContent.length - 1);
+            display.textContent = displayContent;
+        }
+    }
+    else if (buttonContent == ".") {
+        float();
+    }
+    else if (buttonContent == "+/-") {
+        inverseSign();
+    }
+    else {
+        getSecondOperand();
     }
 }))
+
+function displayNumber(button) {
+    if (display.classList.contains("hasContent")) {
+        display.textContent += button.textContent;
+    }
+    else {
+        if (button.textContent == "0") return;
+        display.textContent = button.textContent;
+        display.classList.add("hasContent");
+    }
+    return;
+}
+
+function reset() {
+    display.textContent = "0";
+    display.classList.remove("hasContent");
+    display.classList.remove("isFloat");
+}
+
+function float() {   
+    if (!display.classList.contains("hasContent")) {
+        display.textContent = "0."
+        display.classList.add("hasContent");
+        display.classList.add("isFloat");
+    }
+    else if (display.classList.contains("hasContent") && display.classList.contains("isFloat")) {
+        return;
+    }
+    else {
+        display.textContent += ".";
+        display.classList.add("isFloat");
+    }
+}
+
+function inverseSign() {
+    let n = Number(display.textContent);
+    display.textContent = String((-n));
+}
